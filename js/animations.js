5903b9c05098a42a5a05aaa5308b534cdea9262f
Alexandre <xandygamer126@gmail.com>
2023-09-06 15:01:21 -0300

    Add files via upload

--- js/animations.js
+++ js/animations.js
@@ -11,3 +11,4 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+// finalizado
